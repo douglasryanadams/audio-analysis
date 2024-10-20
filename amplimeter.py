ba6890d6e215b718436cbe5e62628f41d7832300
Douglas Adams <douglasryanadams@gmail.com>
2024-10-20 14:49:15 -0700

    Live audio graph

---
+++ amplimeter.py
@@ -0,0 +1,70 @@
+import queue
+import sys
+
+import numpy as np
+from loguru import logger
+from matplotlib import pyplot as plt
+import sounddevice as sd
+from matplotlib.animation import FuncAnimation
+
+import log
+
+log.setup()
+
+input_device = sd.query_devices(sd.default.device, 'input')
+logger.info("Listening to device: {}", input_device['name'])
+sample_rate = input_device['default_samplerate']
+logger.info("Sample Rate: {}", sample_rate)
+
+length_milliseconds = 200
+downsample = 10
+interval = 30
+
+data_queue = queue.Queue()
+dataset_width = int(length_milliseconds * sample_rate / (1000 * downsample))
+data_points = np.zeros((dataset_width, 1))
+
+
+def audio_callback(indata, frames, time, status):
+    logger.debug("  status: {}", status)
+    logger.debug("  length of indata: {}", len(indata[:]))
+    data_queue.put(indata[::downsample])  # Copies indata
+
+
+def update_plot(frame):
+    global data_points
+    try:
+        while True:
+            try:
+                points = data_queue.get_nowait()
+                logger.debug("  length of points: {}", len(points))
+            except queue.Empty:
+                logger.debug("Queue is empty!")
+                break
+            shift_size = len(points)
+            data_points = np.roll(data_points, -shift_size, axis=0)
+            data_points[-shift_size:, :] = points
+        for column, line in enumerate(lines):
+            line.set_ydata(data_points[:, column])
+        return lines
+    except KeyboardInterrupt:
+        logger.info("Listening complete")
+        sys.exit(0)
+
+
+figure, axes = plt.subplots()
+lines = axes.plot(data_points)
+
+axes.axis((0, len(data_points), -1, 1))
+axes.set_yticks([0])
+axes.yaxis.grid(True)
+axes.tick_params(bottom=False, top=False, labelbottom=False, right=False, left=False, labelleft=False)
+
+figure.tight_layout(pad=0)
+
+with sd.InputStream(device=input_device['index'], channels=1, samplerate=sample_rate, callback=audio_callback):
+    animation_ref = FuncAnimation(figure, update_plot, interval=interval, blit=True)
+    logger.info("Starting RawStream")
+    plt.show()
+
+logger.info("Animation Reference: {}", animation_ref)
--- analyze.py
+++ analyze.py
@@ -1,31 +1,13 @@
-import sys
-import logging
-import inspect
-
 import librosa
 import numpy as np
 from loguru import logger
 import matplotlib.pyplot as plt
 from scipy.signal import decimate
 
-logger.remove()
-logger.add(sys.stdout, level='INFO')
+import log
+log.setup()
 
-class InterceptHandler(logging.Handler):
-    def emit(self, record: logging.LogRecord) -> None:
-        level: str | int
-        try:
-            level = logger.level(record.levelname).name
-        except ValueError:
-            level = record.levelno
 
-        frame, depth = inspect.currentframe(), 0
-        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
-            frame = frame.f_back
-            depth += 1
-
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
-logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
 
 
 logger.info("Loading file")
